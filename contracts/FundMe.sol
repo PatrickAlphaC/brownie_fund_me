96eace70f43ecd9e6bab382beee058de158b4bd8
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2021-09-15 09:11:43 -0400

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -15,7 +15,7 @@ contract FundMe {
     
     // if you're following along with the freecodecamp video
     // Please see https://github.com/PatrickAlphaC/fund_me
-    // to get the starting code, it'll be slightly different than this!
+    // to get the starting solidity contract code, it'll be slightly different than this!
     constructor(address _priceFeed) public {
         priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
