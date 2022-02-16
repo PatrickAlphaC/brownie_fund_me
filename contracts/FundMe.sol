8196249aed0691d614465fc2d88dc8654f797e79
7xAquarius <7xaquarius@gmail.com>
2022-02-16 17:23:16 +0100

    FundMe.sol: round up getEntrancefee() return value

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -56,7 +56,7 @@ contract FundMe {
         uint256 minimumUSD = 50 * 10**18;
         uint256 price = getPrice();
         uint256 precision = 1 * 10**18;
-        return (minimumUSD * precision) / price;
+        return ((minimumUSD * precision) / price) + 1;
     }
 
     modifier onlyOwner() {
